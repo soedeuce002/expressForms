1f0c1048b1ac9d83cc514edc98cb6c6d6e48cafd
Cindi Harris <cmr0319@gmail.com>
2017-07-26 13:54:20 -0400

    set up boilierplates for index.js and form1.mustache....mostly set up at least....

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const mustExp = require("mustache-express");
 const bodyParser = require("body-parser");
 const app = express();
+const formOne = require('./form1');
 
+app.engine('mustache', mustExp());
 app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+
+
+
+
 
 app.get('/', function(req, res){
 
