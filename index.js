bf16299d4ad6ceff0f0d19798ccf93e0bcaeb02d
Cindi Harris <cmr0319@gmail.com>
2017-07-26 11:57:27 -0400

    added all files created for project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mustExp = require("mustache-express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('views', './views');
+
+app.get('/', function(req, res){
+
+
+res.render
+
+});
+
+app.post('/signup', function(req, res){
+
+
+
+});
